ca9339aecbb3d6da7c5c5ec02d7fa68c400e1482
rickschubert <rick@tray.io>
2020-05-18 14:13:18 +0100

    use existing check if it is json schema

--- src/errorHandlingForIncorrectSchemas.ts
+++ src/errorHandlingForIncorrectSchemas.ts
@@ -1,14 +1,9 @@
-
-const checkIfSuppliedSchemaIsActuallyASchema = (schema: any): boolean => {
-    const isAnObjectWithProperties = Object.keys(schema).length > 0
-    const hasTypeProperty = typeof schema.type === "string"
-    return isAnObjectWithProperties && hasTypeProperty
-}
+import { isJsonSchema } from "."
 
 export const errorMsgForIncorrectSchema = `The object you were passing is not a valid JSON schema. Are you sure you constructed it correctly? From experience, users can sometimes forget to also wrap the main parent object into a "strictObject()" function as well.`
 
 export const throwErrorIfNotACorrectJsonSchema = (schema: any) => {
-    const isSchema = checkIfSuppliedSchemaIsActuallyASchema(schema)
+    const isSchema = isJsonSchema(schema)
     if (!isSchema) {
         throw new Error(errorMsgForIncorrectSchema)
     }
